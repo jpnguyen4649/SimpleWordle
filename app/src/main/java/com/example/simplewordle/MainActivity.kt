d690ef36ee6007515f952898233779ed63e7a8c4
jpnguyen4649 <jessica.nguyen100@gmail.com>
2022-09-14 22:27:56 -0700

    Initial commit

---
+++ app/src/main/java/com/example/simplewordle/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.simplewordle
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val wordToGuess = FourLetterWordList.getRandomFourLetterWord()
+        var guess = (findViewById<View>(R.id.guessInput) as EditText).text.toString()
+        val submit = findViewById<Button>(R.id.Guess)
+    }
+
+    submit.setOnClickListener{
+
+    }
+    /**
+     * Parameters / Fields:
+     *   wordToGuess : String - the target word the user is trying to guess
+     *   guess : String - what the user entered as their guess
+     *
+     * Returns a String of 'O', '+', and 'X', where:
+     *   'O' represents the right letter in the right place
+     *   '+' represents the right letter in the wrong place
+     *   'X' represents a letter not in the target word
+     */
+    private fun checkGuess(guess: String) : String {
+        var result = ""
+        for (i in 0..3) {
+            if (guess[i] == wordToGuess[i]) {
+                result += "O"
+            }
+            else if (guess[i] in wordToGuess) {
+                result += "+"
+            }
+            else {
+                result += "X"
+            }
+        }
+        return result
+    }
+
+
+
+
+}
