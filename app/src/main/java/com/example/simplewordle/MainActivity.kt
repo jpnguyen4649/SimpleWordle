d35441b48ba84a9e485e9addf795dadaf9e0c48e
jpnguyen4649 <jessica.nguyen100@gmail.com>
2022-09-16 20:51:44 -0700

    Added basic functionality

--- app/src/main/java/com/example/simplewordle/MainActivity.kt
+++ app/src/main/java/com/example/simplewordle/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.simplewordle
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -7,21 +8,53 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+    val wordToGuess = FourLetterWordList.getRandomFourLetterWord()
+    var guess = ""
+    var attempts = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val wordToGuess = FourLetterWordList.getRandomFourLetterWord()
-        var guess = (findViewById<View>(R.id.guessInput) as EditText).text.toString()
         val submit = findViewById<Button>(R.id.Guess)
+
+        submit.setOnClickListener{
+            try{
+                guess = (findViewById<View>(R.id.guessInput) as EditText).text.toString().uppercase()
+                (findViewById<View>(R.id.guessInput) as EditText).setText("")
+                when (attempts){
+                    0 -> {
+                        findViewById<TextView>(R.id.guess1).text = guess
+                        findViewById<TextView>(R.id.result1).text = checkGuess(guess)
+                        attempts+=1
+                        findViewById<TextView>(R.id.resultsText).text = "${3-attempts} guesses left"
+                    }
+                    1 -> {
+                        findViewById<TextView>(R.id.guess2).text = guess
+                        findViewById<TextView>(R.id.result2).text = checkGuess(guess)
+                        attempts+=1
+                        findViewById<TextView>(R.id.resultsText).text = "${3-attempts} guesses left"
+                    }
+                    2 -> {
+                        findViewById<TextView>(R.id.guess3).text = guess
+                        findViewById<TextView>(R.id.result3).text = checkGuess(guess)
+                        attempts+=1
+                        findViewById<TextView>(R.id.resultsText).text = "${3-attempts} guesses left"
+                        findViewById<TextView>(R.id.messageText).text = "You are out of guesses! The answer was ${wordToGuess}"
+                        submit.isClickable = false
+                        submit.setBackgroundColor(Color.GRAY)
+                    }
+                }
+            }catch (t: Throwable){
+                android.widget.Toast.makeText(this, "INPUT ERROR", Toast.LENGTH_SHORT).show()
+            }
+            //used this line in testing to see answer
+            //findViewById<TextView>(R.id.guess1).text = wordToGuess
+        }
     }
 
-    submit.setOnClickListener{
-
-    }
     /**
      * Parameters / Fields:
      *   wordToGuess : String - the target word the user is trying to guess
@@ -47,8 +80,5 @@ class MainActivity : AppCompatActivity() {
         }
         return result
     }
-
-
-
-
 }
+
